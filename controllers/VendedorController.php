9c5271863ed23639d59635cae3000cb26679564f
Blairi <axel.avi.fer@gmail.com>
2022-02-21 14:56:27 -0600

    Iniciando repositorio

---
+++ controllers/VendedorController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Controllers;
+
+class VendedorController{
+    public static function crear(){
+        echo "creando vendedor";
+    }
+
+    public static function actualizar(){
+        echo "actualizando vendedor";
+    }
+
+
+    public static function eliminar(){
+        echo "eliminando";
+    }
+}
--- views/propiedades/admin.php
+++ views/propiedades/admin.php
@@ -15,7 +15,7 @@
 
 
     <a href="/propiedades/crear" class="boton-verde">Nueva Propiedad</a>
-    <a href="/admin/vendedores/crear.php" class="boton-amarillo">Nuevo Vendedor</a>
+    <a href="/vendedores/crear" class="boton-amarillo">Nuevo Vendedor</a>
     <h2>Propiedades</h2>
 
     <table class="propiedades">
@@ -51,4 +51,42 @@
             <?php endforeach; ?>
         </tbody>
     </table>
+
+    <h2>Vendedores</h2>
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Telefono</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody><!-- Mostrar los Resultados -->
+
+                <?php foreach( $vendedores as $vendedor ): ?>
+                <tr>
+                    <td><?php echo $vendedor->id; ?></td>
+                    <td><?php echo $vendedor->nombre . " " . $vendedor->apellido; ?></td>
+                    <td><?php echo $vendedor->telefono; ?></td>
+                    <td>
+                        <form method="POST" class="w-100">
+
+                            <input type="hidden" 
+                            name="id" 
+                            value="<?php echo $vendedor->id; ?>">
+                            <input 
+                            type="hidden" 
+                            name="tipo" 
+                            value="vendedor">
+
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        <a href="/vendedores/actualizar.php?id=<?php echo $vendedor->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 </main>
